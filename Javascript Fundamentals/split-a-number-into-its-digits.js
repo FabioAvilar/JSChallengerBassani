158661775726987e2df981c0c2ea62a4d9df5b71
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-08 18:01:46 +0200

    ex split a number into its digits

---
+++ Javascript Fundamentals/split-a-number-into-its-digits.js
@@ -0,0 +1,25 @@
+// Write a function that takes a number (a) as argument
+// Split a into its individual digits and return them in an array
+// Tipp: you might want to change the type of the number for the splitting
+
+function myFunction(a) {
+
+    return [...a+''].map(v => +v);
+}
+
+console.log(myFunction(10));
+
+console.log(myFunction(931));
+
+
+// or we can make this
+
+function myFunction(a) {
+    const string = a + '';
+    const strings = string.split('');
+    return strings.map(digit => Number(digit));
+}
+
+console.log(myFunction(10));
+
+console.log(myFunction(931));

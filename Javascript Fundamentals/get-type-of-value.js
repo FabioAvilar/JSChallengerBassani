22b4541d937be31361fe4aa0061f96cf61ec6e55
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-09 19:01:23 +0200

    get type of value

---
+++ Javascript Fundamentals/get-type-of-value.js
@@ -0,0 +1,13 @@
+// Write a function that takes a value as argument
+// Return the type of the value
+
+function myFunction(a) {
+
+    return typeof a;
+}
+
+console.log(myFunction(1));
+
+console.log(myFunction({}));
+
+console.log(myFunction("string"));

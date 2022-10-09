f5b8dbd953c3be5aaf66e5c0da0c06ac112d8a2e
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-09 19:08:02 +0200

    sum two numbers

---
+++ Javascript Fundamentals/sum-two-numbers.js
@@ -0,0 +1,15 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+
+function myFunction(a, b) {
+    var sum = a + b;
+    return sum;
+}
+
+
+console.log(myFunction(1, 2));
+
+console.log(myFunction(1, 10));
+
+console.log(myFunction(99, 1));

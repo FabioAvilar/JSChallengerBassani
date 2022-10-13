95ba55d73683f0ca7c0c88049e8dfbe1bf485670
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-13 09:08:50 +0200

    check if two dates are within 1 hour from each other

---
+++ javascript Dates/check-if-two-dates-are-within-1-hour-from-each-other.js
@@ -0,0 +1,25 @@
+// Write a function that takes two date instances as argument
+// It should return true if the difference between the dates is less than or equal to 1 hour
+// It should return false otherwise
+
+function myFunction(a, b) {
+
+    return Math.abs(a - b) <= 60 * 60 * 1000;
+}
+
+
+console.log(
+    myFunction(new Date("2000/01/01 08:00:00"), new Date("2000/01/01 08:45:00"))
+);
+
+console.log(
+    myFunction(new Date("2000/01/01 09:00:00"), new Date("2000/01/01 08:45:00"))
+);
+
+console.log(
+    myFunction(new Date("2000/01/01 08:00:00"), new Date("2000/01/01 09:45:00"))
+);
+
+console.log(
+    myFunction(new Date("2000/01/01 08:00:00"), new Date("2000/01/01 09:00:00"))
+);

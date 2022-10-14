4cd939768c62cd2977e997e597a0728cae01d43c
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-14 08:23:58 +0200

    get union of two sets

--- javascript Sets/get-union-of-two-sets.js
+++ javascript Sets/get-union-of-two-sets.js
@@ -15,3 +15,34 @@ console.log(myFunction(new Set([1, 2, 3]), new Set([3, 4, 5])));
 console.log(
     myFunction(new Set([false, false, NaN]), new Set([true, true, NaN]))
 );
+
+// or we can solve like this
+
+function myFunction(a, b) {
+    for (let el of b) a.add(el);
+    return a;
+}
+
+console.log(myFunction(new Set("123"), new Set("234")));
+
+console.log(myFunction(new Set([1, 2, 3]), new Set([3, 4, 5])));
+
+console.log(
+    myFunction(new Set([false, false, NaN]), new Set([true, true, NaN]))
+);
+
+// or also we can solve like this
+
+function myFunction(a, b) {
+    const result = new Set(a);
+    b.forEach((el) => result.add(el));
+    return result;
+}
+
+console.log(myFunction(new Set("123"), new Set("234")));
+
+console.log(myFunction(new Set([1, 2, 3]), new Set([3, 4, 5])));
+
+console.log(
+    myFunction(new Set([false, false, NaN]), new Set([true, true, NaN]))
+);

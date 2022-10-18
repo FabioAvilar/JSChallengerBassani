7bd6180ea74b83c4ad4886c0f38928f94c84c82e
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-18 08:24:50 +0200

    remove element from the DOM

--- javascript DOM/remove-element-from-the-DOM/script.js
+++ javascript DOM/remove-element-from-the-DOM/script.js
@@ -1,13 +1,12 @@
 // Remove element from the DOM
 // You may not see it in the example UI, but underneath the red circle is a green circle. Extend the function removeRedCircle to remove the circle with id red from the DOM.
 // Make sure that you really remove the element instead of just hiding it. Confirm that your code works by clicking the button.
-// const element = document.querySelector("#element");
 
-const toggleColor = (isEntering) => {
-    element.style.background = isEntering ? "orange" : "black";
+const button = document.querySelector("#button");
+
+const removeRedCircle = () => {
+    const redCircle = document.querySelector("#red");
+    redCircle.parentNode.removeChild(redCircle);
 };
 
-// type in your code here
-element.addEventListener("mouseover", () => toggleColor(true));
-element.addEventListener("mouseleave", () => toggleColor(false));
-
+button.addEventListener("click", removeRedCircle);

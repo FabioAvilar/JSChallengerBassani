2afceb2dd4ecae5ab3a1cd7333c41b684a606eb0
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-18 08:03:32 +0200

    cursor enters and leaves element

---
+++ javascript DOM/cursor-enters-and-leaves-element/script.js
@@ -0,0 +1,10 @@
+const element = document.querySelector("#element");
+
+const toggleColor = (isEntering) => {
+    element.style.background = isEntering ? "orange" : "black";
+};
+
+// type in your code here
+element.addEventListener("mouseover", () => toggleColor(true));
+element.addEventListener("mouseleave", () => toggleColor(false));
+

fbffc442d2112286223c493df58abb7b34f6168d
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-19 07:59:11 +0200

    check the checkbox

---
+++ javascript DOM/check-the-checkbox/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+    // type in your code here
+    const checkbox = document.querySelector("#checkbox");
+    checkbox.checked = true;
+});
